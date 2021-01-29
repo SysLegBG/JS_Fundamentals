2c8331d7b421ec76849785522b9763b89c52cccd
SysLegBG <sysleg@abv.bg>
2021-01-29 17:52:43 +0200

    function, which prints all unique pairs in an array of integers whose sum is equal to a given number.

---
+++ Exercise - Arrays/magicSum.js
@@ -0,0 +1,17 @@
+function magicSum(arr, n) {
+    for (let i = 0; i < arr.length; i++) {
+        const el = arr[i];
+        for (let j = i + 1; j < arr.length; j++) {
+            const elj = arr[j];
+            if ((el + elj) == n) {
+                let pair = [];
+                pair.push(el, elj);
+                console.log(pair.join(" "));
+            }
+        }
+    }
+}
+
+//magicSum([1, 7, 6, 2, 19, 23], 8)
+//magicSum([14, 20, 60, 13, 7, 19, 8], 27)
+magicSum([1, 2, 3, 4, 5, 6], 6)
