c055549ff91c8e454e7583f7d73a531f3ab15484
SysLegBG <sysleg@abv.bg>
2021-01-29 17:44:25 +0200

    a function which receives two string arrays and merges them into a third array. •	If the index of the element is even, add into the third array the sum of both elements at that index •	If the index of the element is odd, add the concatenation of both elements at that index

---
+++ Exercise - Arrays/mergeArr.js
@@ -0,0 +1,17 @@
+function mergeArr(fArr, sArr){
+    let finalArr = [];
+
+    for (let i = 0; i < fArr.length; i++) {
+        if(i % 2 == 0){
+            finalArr.push(Number(fArr[i]) + Number(sArr[i]));
+        }else{
+            finalArr.push(fArr[i] + sArr[i]);
+        }
+    }
+
+    console.log(finalArr.join(" - "));
+}
+
+mergeArr(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11']
+)
