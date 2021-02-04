235d922b4b560f5c1a3fbd1aba5e7836e732ec12
SysLegBG <sysleg@abv.bg>
2021-02-04 12:41:03 +0200

    Function that stores information about a person’s name and his phone number.

---
+++ Lab - Associative Arrays/phoneBook.js
@@ -0,0 +1,17 @@
+function phoneBook(data) {
+    let phoneBook = {};
+
+    data.forEach(line => {
+        const [name, phone] = line.split(" ");
+        phoneBook[name] = phone;
+    })
+
+    for (const name in phoneBook) {
+        console.log(`${name} -> ${phoneBook[name]}`);
+    }
+}
+
+phoneBook(['Tim 0834212554',
+    'Peter 0877547887',
+    'Bill 0896543112',
+    'Tim 0876566344'])
