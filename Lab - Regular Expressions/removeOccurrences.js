3c4126e4dec4fc84cbfdcb2915698bf0e7cf0080
SysLegBG <sysleg@abv.bg>
2021-02-13 21:46:55 +0200

    Function that receives a text and a word to remove all occurrences of it inside the text.

---
+++ Lab - Regular Expressions/removeOccurrences.js
@@ -0,0 +1,9 @@
+function removeOccurrences(word, text) {
+    while(text.includes(word)){
+        text = text.replace(word, "")
+    }
+
+    console.log(text);
+}
+
+removeOccurrences('ice', 'kicegiciceeb')
